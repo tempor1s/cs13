34d888ee5208314ce00e53b48c49faa2cbd20228
Ben <benlaugherty@gmail.com>
2020-01-29 19:59:01 -0800

    [add] find all indexes

--- Code/strings.py
+++ Code/strings.py
@@ -34,18 +34,25 @@ def find_all_indexes(text, pattern):
     or an empty list if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+
+    if pattern == '':
+        return [i for i in range(len(text))]
+
+    indexes = []
+    for i, character in enumerate(text):
+        if character == pattern[0]:
+            if text[i:i + len(pattern)] == pattern:
+                indexes.append(i)
+    return indexes
 
 
 def test_string_algorithms(text, pattern):
     # found = contains(text, pattern)
     # print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
-    # TODO: Uncomment these lines after you implement find_index
     index = find_index(text, pattern)
     print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
-    # TODO: Uncomment these lines after you implement find_all_indexes
-    # indexes = find_all_indexes(text, pattern)
-    # print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
+    indexes = find_all_indexes(text, pattern)
+    print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
 
 
 def main():
