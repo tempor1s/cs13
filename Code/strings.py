9b5307c82063a05ebac72bb4ccd978e093af4c8c
Ben <benlaugherty@gmail.com>
2020-01-29 20:02:36 -0800

    [add] really easy contains lol

--- Code/strings.py
+++ Code/strings.py
@@ -5,7 +5,7 @@ def contains(text, pattern):
     """Return a boolean indicating whether pattern occurs in text."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # TODO: Implement contains here (iteratively and/or recursively)
+    return pattern in text
 
 
 def find_index(text, pattern):
